8ab0f65fe9594fb7011a41bc9d66473837597525
Joe Mahoney <jmahoney@iterative.co.nz>
2015-10-09 10:19:12 +1300

    wip

--- thread.js
+++ thread.js
@@ -4,7 +4,7 @@ var morality = require('verbal-morality');
 var bullshit = require('industry-bullshit');
 var https = require('https'),
     fs = require('fs');
-    
+
 app.use(express.static('public'));
 
 app.get('/level', (req, res) => {
@@ -29,7 +29,7 @@ app.get('/level', (req, res) => {
 
 app.get('/listen', (req, res) => {
   let moralityLevel = morality(req.query.term);
-  let moralityFine = moralityLevel > 1 ? moralityLevel : 0;
+  let moralityFine = moralityLevel;
   let moralityText = moralityLevel > 3 ? "intolerably immoral" : "unnacceptably immoral";
   let bullshitScore = bullshit.score(req.query.term);
   let bullshitText = bullshitScore > 3 ? "is complete nonsense" : "sounds a bit nonsensical";
